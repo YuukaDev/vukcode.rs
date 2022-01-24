583c7f0cd1cbfd865ff60623463cc993730da8f9
YuukaDev <vule.amv1@gmail.com>
2022-01-24 21:48:11 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import customTheme from "../theme/theme";
+import "../styles/style.css";
+import { extendTheme } from "@chakra-ui/react"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={customTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
