775f470c0176fbf67cbbab13522e533be59781b2
YuukaDev <vule.amv1@gmail.com>
2022-01-25 21:47:49 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import DarkMode from "../components/DarkMode/DarkMode"
 import ImageNext from "next/image";
 
 import manOne from "../public/man1.svg";
-import manTwo from "../public/man2.svg";
+//import manTwo from "../public/man2.svg";
 
 export default function Home() {
   return (
