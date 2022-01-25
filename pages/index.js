91255bfec4c1e53edd49cda73fb80aeba8a5eaf6
YuukaDev <vule.amv1@gmail.com>
2022-01-25 21:47:41 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import DarkMode from "../components/DarkMode/DarkMode"
 import ImageNext from "next/image";
-import { Image } from "@chakra-ui/react";
 
 import manOne from "../public/man1.svg";
 import manTwo from "../public/man2.svg";
