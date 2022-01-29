ed38f593ba28990df0b7dc3e8e127a6d9825fa1b
YuukaDev <vule.amv1@gmail.com>
2022-01-29 21:40:14 +0100

    Update Card.js

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -28,6 +28,7 @@ export default function Card() {
                             <Icon as={FiGithub} />
                         </Link>
                         <Link borderRadius="lg" display="flex" justifyContent="center" alignItems="center" gap="5px" _hover={{
+                            boxShadow: "4px 4px #ff4000",
                             textDecoration: "none"
                         }} border="1px solid #ff4000" padding="10px">
                             Preview
@@ -50,6 +51,12 @@ export default function Card() {
                             An android application through which one can scan his/her bill and all the products will be scanned, categorised and will be added to his/her monthly expenses. This app also shows top 100 crypto currencies and clicking on particular crypto all the information (along with chart) will be displayed.
                         </Text>
                     </Flex>
+                    <Box>
+                        <Flex>
+                            <Icon as={FiGithub} />
+                            <Icon as={AiOutlineLink} />
+                        </Flex>
+                    </Box>
                 </Box>
             </Box>
 
@@ -64,6 +71,22 @@ export default function Card() {
                         </Text>
                         <Image width="310px" height="294px" src="https://portfolio-neelbavarva.vercel.app/static/media/FinanceTracker.06457114.jpeg" />
                     </Flex>
+                    <Box display="flex" gap="15px" color="#ff4000" mt="-20px">
+                        <Link borderRadius="lg" display="flex" justifyContent="center" alignItems="center" gap="5px" _hover={{
+                            boxShadow: "4px 4px #ff4000",
+                            textDecoration: "none"
+                        }} border="1px solid #ff4000" padding="10px">
+                            GitHub
+                            <Icon as={FiGithub} />
+                        </Link>
+                        <Link borderRadius="lg" display="flex" justifyContent="center" alignItems="center" gap="5px" _hover={{
+                            boxShadow: "4px 4px #ff4000",
+                            textDecoration: "none"
+                        }} border="1px solid #ff4000" padding="10px">
+                            Preview
+                            <Icon as={AiOutlineLink} />
+                        </Link>
+                    </Box>
                 </Box>
             </Box>
 
