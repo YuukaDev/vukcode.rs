f4d9348c3f581a152714329fbaa3033d202e0cc8
YuukaDev <vule.amv1@gmail.com>
2022-01-29 21:37:52 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,6 @@ import "../styles/style.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import customTheme from "../theme/theme";
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-
-
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={customTheme}>
