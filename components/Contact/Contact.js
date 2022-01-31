28c85e17911f69b29869c52e4a25084b3f1c37a8
YuukaDev <vule.amv1@gmail.com>
2022-01-31 19:27:54 +0100

    Update Contact.js

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -1,11 +1,11 @@
-import { Box, Heading, Text, VStack } from "@chakra-ui/react";
+import { Box, Heading, VStack } from "@chakra-ui/react";
 import Form from "../Form/Form";
 
 export default function Contact() {
     return (
         <VStack margin="0" id="contact" mb="100px">
             <Box width="35%" float="left">
-                <Heading>
+                <Heading mb="1em" textAlign="center">
                     You Can Contact Me Here
                 </Heading>
                 <Box>
