464a6432d509cbf8ca1ef93d3d4a247d34970adf
YuukaDev <vule.amv1@gmail.com>
2022-02-01 12:31:18 +0100

    Update Form.js

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -5,30 +5,32 @@ import { useToast } from "@chakra-ui/react";
 
 export default function FormComp() {
     const toast = useToast();
-    const [email, setEmail] = useState("");
-    const [name, setName] = useState("");
-    const [subject, setSubject] = useState("");
+    //const [email, setEmail] = useState("");
+    //const [name, setName] = useState("");
+    //const [subject, setSubject] = useState("");
     return (
         <VStack>
             <form action="https://formsubmit.co/0a659b40cd7c824cd255011d92034184" method="POST">
                 <Box width="400px">
                     <input type="hidden" name="_next" value="http://localhost:3000/" />
                     <input type="hidden" name="_captcha" value="false" />
-                    <input
+                    <Input
+                        mb="10px"
                         variant="flushed"
                         type="text"
                         placeholder="Name"
                         size="lg"
                         onChange={(e) => setName(e.target.value)}
                     />
-                    <input
+                    <Input
+                        mb="10px"
                         variant="flushed"
                         type="email"
                         placeholder="Email Adress"
                         size="lg"
                         onChange={(e) => setEmail(e.target.value)}
                     />
-                    <textarea
+                    <Textarea
                         variant="flushed"
                         placeholder="Message"
                         size="lg"
