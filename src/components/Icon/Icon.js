ea66e5d90b0a481aafb97ca627550736ac3610c4
YuukaDev <vule.amv1@gmail.com>
2022-02-08 01:17:50 +0100

    Update Icon.js

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 
 export default function Icon() {
     return (
-        <Box mt="100px" className="animation">
+        <Box mt={{ sm: "50px", xl: "100px" }} className="animation">
             <div class="arrow">
                 <a href="#projects">
                     <span>
