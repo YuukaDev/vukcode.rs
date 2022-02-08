28bf81698eab91db5cd922089b192d0a2e6faebe
YuukaDev <vule.amv1@gmail.com>
2022-02-08 01:17:51 +0100

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,8 +4,19 @@ import Card from "../Card/Card";
 export default function Projects() {
     return (
         <VStack margin="0" id="projects">
-            <Box mt="5%" width="35%" float="left">
-                <Heading>Featured Projects 👨‍💻</Heading>
+            <Box mt={{
+                sm: "20%",
+                xl: "5%"
+            }} width={{
+                sm: "90%",
+                md: "50%",
+                lg: "50%",
+                xl: "35% ",
+            }} float="left">
+                <Heading fontSize={{
+                    sm: "2em",
+                    xl: "2.25rem"
+                }}>Featured Projects 👨‍💻</Heading>
             </Box>
             <Card />
         </VStack>
