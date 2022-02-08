8e7508009ff6d77a9abace57c5b5de375346fbaa
YuukaDev <vule.amv1@gmail.com>
2022-02-08 01:17:46 +0100

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,8 +4,19 @@ import Form from "../Form/Form";
 export default function Contact() {
     return (
         <VStack margin="0" id="contact" mb="50px">
-            <Box width="35%" float="left">
-                <Heading mb="0.7em" textAlign="center">
+            <Box
+                width={{
+                    sm: "110%",
+                    xl: "35%"
+                }}
+                float="left">
+                <Heading
+                    fontSize={{
+                        sm: "1.7em",
+                        xl: "2.25rem"
+                    }}
+                    mb="0.7em"
+                    textAlign="center">
                     You Can Contact Me Here
                 </Heading>
                 <Box>
