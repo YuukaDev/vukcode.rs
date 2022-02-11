a0e036c5ac4f513b31d1a80770178da4b2250f29
YuukaDev <vule.amv1@gmail.com>
2022-02-11 23:45:26 +0100

    Update Projects.js

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -22,78 +22,9 @@ export default function Projects() {
                 }}>Featured Projects 👨‍💻</Heading>
                 <Divider width={{ sm: "70%", xl: "52%" }} mt="15px" mb="10px" bg="#ff4000" padding="1px" />
             </Box>
-            <Box width={{
-                sm: "85%",
-                md: "50%",
-                lg: "50%",
-                xl: "35% ",
-            }} float="left" mt={{
-                sm: "50px",
-                lg: "150px",
-                xl: "100px"
-            }}>
-                <Box>
-                    <br />
-                    <Grid width="700px" gridTemplateColumns="repeat(2, 2fr)" gap="25px">
-                        <Box
-                            transition="0.5s all ease"
-                            _hover={{
-                                transition: "0.5s all ease",
-                                transform: "scale(1.05)"
-                            }}
-                            boxShadow="dark-lg">
-                            <Link href="https://github.com/YuukaDev/Xquisite"><img src="https://gh-card.dev/repos/YuukaDev/Xquisite.svg" /></Link>
-                        </Box>
-                        <Box
-                            transition="0.5s all ease"
-                            _hover={{
-                                transition: "0.5s all ease",
-                                transform: "scale(1.05)"
-                            }}
-                            bg="#fff"
-                            boxShadow="dark-lg">
-                            <Link href="https://github.com/YuukaDev/Foody"><img src="https://gh-card.dev/repos/YuukaDev/Foody.svg" /></Link>
-                        </Box>
-                        <Box
-                            transition="0.5s all ease"
-                            _hover={{
-                                transition: "0.5s all ease",
-                                transform: "scale(1.05)"
-                            }}
-                            boxShadow="dark-lg">
-                            <Link href="https://github.com/YuukaDev/GitHub-Searcher"><img src="https://gh-card.dev/repos/YuukaDev/GitHub-Searcher.svg" /></Link>
-                        </Box>
-                        <Box
-                            transition="0.5s all ease"
-                            _hover={{
-                                transition: "0.5s all ease",
-                                transform: "scale(1.05)"
-                            }}
-                            boxShadow="dark-lg">
-                            <Link href="https://github.com/YuukaDev/Pokedex"><img src="https://gh-card.dev/repos/YuukaDev/Pokedex.svg" /></Link>
-                        </Box>
-                        <Box
-                            transition="0.5s all ease"
-                            _hover={{
-                                transition: "0.5s all ease",
-                                transform: "scale(1.05)"
-                            }}
-                            boxShadow="dark-lg"
-                        >
-                            <Link href="https://github.com/YuukaDev/Anime-Quotes"><img src="https://gh-card.dev/repos/YuukaDev/Anime-Quotes.svg" /></Link>
-                        </Box>
-                        <Box
-                            transition="0.5s all ease"
-                            _hover={{
-                                transition: "0.5s all ease",
-                                transform: "scale(1.05)"
-                            }}
-                            boxShadow="dark-lg">
-                            <Link href="https://github.com/YuukaDev/Aethersio"><img src="https://gh-card.dev/repos/YuukaDev/Aethersio.svg" /></Link>
-                        </Box>
-                    </Grid>
-                </Box>
-            </Box >
+            <Box>
+                <Card />
+            </Box>
         </VStack >
     )
 }
