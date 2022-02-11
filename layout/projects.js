58bd0d5ca40d6b6656b2fb6dc22d556ddf0aa9fe
YuukaDev <vule.amv1@gmail.com>
2022-02-11 23:45:28 +0100

    Update projects.js

--- layout/projects.js
+++ layout/projects.js
@@ -1,11 +1,11 @@
 import Navigation from "../components/Navigation/Navigation";
-import ProjectsComp from "../components/Projects/Projects";
+import ProjectsComponent from "../components/Projects/Projects";
 
 export default function Projects() {
     return (
         <div>
             <Navigation />
-            <ProjectsComp />
+            <ProjectsComponent />
         </div>
     )
 }
