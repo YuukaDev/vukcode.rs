d90364404b537f75fcd2ac638c73b8a9cee34ecf
YuukaDev <vule.amv1@gmail.com>
2022-02-18 23:39:19 +0100

    Update Navigation.js

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,3 +1,4 @@
+import NextLink from "next/link";
 import { Link, Button, HStack } from "@chakra-ui/react";
 import DarkMode from "../DarkMode/DarkMode";
 
@@ -12,82 +13,87 @@ export default function Navigation() {
                 py={5}
             >
                 <HStack alignItems="center" spacing={{ base: 0, md: 2 }}>
-                    <Link
-                        href="/"
-                        transition="0.5s all ease"
-                        textDecoration="none"
-                        _hover={{
-                            transition: "0.5s all ease",
-                            transform: "scale(1.1)",
-                            textDecoration: "none",
-                        }}
-                    >
-                        <Button fontSize={{
-                            sm: "1em",
-                            md: "0.5em",
-                            lg: "1em",
-                            xl: "1.2em",
-                        }} size="md" variant="ghost">
-                            Home
-                        </Button>
-                    </Link>
-                    <Link
-                        href="/#about"
-                        transition="0.5s all ease"
-                        textDecoration="none"
-                        _hover={{
-                            transition: "0.5s all ease",
-                            transform: "scale(1.1)",
-                            textDecoration: "none",
-                        }}
-                    >
-                        <Button fontSize={{
-                            sm: "1em",
-                            md: "0.5em",
-                            lg: "1em",
-                            xl: "1.2em",
-                        }} size="md" variant="ghost">
-                            About
-                        </Button>
-                    </Link>
-                    <Link
-                        href="/projects"
-                        transition="0.5s all ease"
-                        textDecoration="none"
-                        _hover={{
-                            transition: "0.5s all ease",
-                            transform: "scale(1.1)",
-                            textDecoration: "none",
-                        }}
-                    >
-                        <Button fontSize={{
-                            sm: "1em",
-                            md: "0.5em",
-                            lg: "1em",
-                            xl: "1.2em",
-                        }} size="md" variant="ghost">
-                            Projects
-                        </Button>
-                    </Link>
-                    <Link
-                        href="/#contact"
-                        transition="0.5s all ease"
-                        textDecoration="none"
-                        _hover={{
-                            transition: "0.5s all ease",
-                            transform: "scale(1.1)",
-                            textDecoration: "none",
-                        }}
-                    >
-                        <Button fontSize={{
-                            sm: "1em",
-                            md: "0.5em",
-                            lg: "1em",
-                            xl: "1.2em",
-                        }} size="md" variant="ghost">
-                            Contact
-                        </Button>
-                    </Link>
+                    <NextLink href="/" passHref>
+                        <Link
+                            href="/"
+                            transition="0.5s all ease"
+                            textDecoration="none"
+                            _hover={{
+                                transition: "0.5s all ease",
+                                transform: "scale(1.1)",
+                                textDecoration: "none",
+                            }}
+                        >
+                            <Button fontSize={{
+                                sm: "1em",
+                                md: "0.5em",
+                                lg: "1em",
+                                xl: "1.2em",
+                            }} size="md" variant="ghost">
+                                Home
+                            </Button>
+                        </Link>
+                    </NextLink>
+                    <NextLink href="/#about" passHref>
+                        <Link
+                            transition="0.5s all ease"
+                            textDecoration="none"
+                            _hover={{
+                                transition: "0.5s all ease",
+                                transform: "scale(1.1)",
+                                textDecoration: "none",
+                            }}
+                        >
+                            <Button fontSize={{
+                                sm: "1em",
+                                md: "0.5em",
+                                lg: "1em",
+                                xl: "1.2em",
+                            }} size="md" variant="ghost">
+                                About
+                            </Button>
+                        </Link>
+                    </NextLink>
+                    <NextLink href="/projects" passHref>
+                        <Link
+                            transition="0.5s all ease"
+                            textDecoration="none"
+                            _hover={{
+                                transition: "0.5s all ease",
+                                transform: "scale(1.1)",
+                                textDecoration: "none",
+                            }}
+                        >
+                            <Button fontSize={{
+                                sm: "1em",
+                                md: "0.5em",
+                                lg: "1em",
+                                xl: "1.2em",
+                            }} size="md" variant="ghost">
+                                Projects
+                            </Button>
+                        </Link>
+                    </NextLink>
+                    <NextLink href="/#contact" passHref>
+                        <Link
+                            transition="0.5s all ease"
+                            textDecoration="none"
+                            _hover={{
+                                transition: "0.5s all ease",
+                                transform: "scale(1.1)",
+                                textDecoration: "none",
+                            }}
+                        >
+                            <Button fontSize={{
+                                sm: "1em",
+                                md: "0.5em",
+                                lg: "1em",
+                                xl: "1.2em",
+                            }} size="md" variant="ghost">
+                                Contact
+                            </Button>
+                        </Link>
+                    </NextLink>
                     <Link
                         transition="0.5s all ease"
                         textDecoration="none"
