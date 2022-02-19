6f547acc5bc626c01ac195866593956773fdc4ba
YuukaDev <vule.amv1@gmail.com>
2022-02-19 01:58:53 +0100

    Update

--- layout/projects.js
+++ layout/projects.js
@@ -1,12 +1,16 @@
 import { Box } from "@chakra-ui/react";
+import { PageTransition } from "next-page-transitions";
+
 import Navigation from "../components/Navigation/Navigation";
 import ProjectsComponent from "../components/Projects/Projects";
 
 export default function Projects() {
     return (
-        <Box height="100%">
-            <Navigation />
-            <ProjectsComponent />
-        </Box>
+        <PageTransition timeout={300}>
+            <Box height="100%">
+                <Navigation />
+                <ProjectsComponent />
+            </Box>
+        </PageTransition>
     )
 }
