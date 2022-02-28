a8d70298cdb644b9d57f68a288168acabf031499
YuukaDev <vule.amv1@gmail.com>
2022-02-28 19:27:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const port = process.env.port || 3000;
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
 
-
 app.prepare().then(() => {
     createServer(async (req, res) => {
         try {
